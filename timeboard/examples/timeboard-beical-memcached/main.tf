8c243e64642b194fa60d536635733a8a58d1deaa
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-05-30 12:56:48 +0700

    Reformat repo structure. Add env var (#2)

--- timeboard/examples/timeboard-beical-memcached/main.tf
+++
@@ -1,5 +0,0 @@
-module "timeboard_memcached_beical-memcached" {
-  source         = "../../"
-  product_domain = "BEI"
-  cluster        = "beical-memcached"
-}
--- timeboard/examples/timeboard-beical-memcached/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "timeboard_memcached_beical-memcached_title" {
-  value = "${module.timeboard_memcached_beical-memcached.title}"
-}
